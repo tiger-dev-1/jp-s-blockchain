2f1ee8eec2ff50a1a2eae8bd73a752910037c1fa
JP <jota.tiger.dev@gmail.com>
2024-11-16 19:20:46 -0300

    separated the functions so the code can be more readable

--- blockchain.py
+++ blockchain.py
@@ -1,9 +1,14 @@
 blockchain = [[1]]
 
-def add_value():
-    blockchain.append([blockchain[-1], 5.3])
-    print(blockchain)
+def get_last_blockchain_value():
+    return blockchain[-1]
 
-add_value()
-add_value()
-add_value()
+
+def add_value(transaction_amount):
+    blockchain.append([get_last_blockchain_value(), transaction_amount])
+
+add_value(2)
+add_value(0.9)
+add_value(10.8)
+
+print(blockchain)
